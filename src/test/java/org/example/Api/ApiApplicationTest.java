409964732f9563af1a46866db8878a5d7bef67ed
Potts Lavada <pottslav@pr-a6.kzn.21-school.ru>
2024-12-05 19:40:51 +0300

    upload

---
+++ src/test/java/org/example/Api/ApiApplicationTest.java
@@ -0,0 +1,12 @@
+package org.example.Api;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
